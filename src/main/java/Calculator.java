9f329b29dae0dd9ce1552134b2eaad4dfb250a09
Vladislav Khomchenkov <vladdiablo1@mail.ru>
2021-01-30 18:07:44 +0300

    1

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,13 @@
+public class Calculator {
+    public double addition(int var1, double var2) {
+        return 0;
+    }
+
+    public int multiplication(int var1, int var2) {
+        return 0;
+    }
+
+    public double multiplicationAll(int var1, int var2, double var3) {
+        return 0;
+    }
+}
